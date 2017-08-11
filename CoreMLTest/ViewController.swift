cc6bb996f1f3ba81873ee72d324c40b3877ff78f
Leconize <g.supavit@gmail.com>
2017-08-11 22:19:02 +0700

    rearage file

--- CoreMLTest/ViewController.swift
+++ CoreMLTest/ViewController.swift
@@ -17,28 +17,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nil
     
-        guard let mlMultiArray1 = try? MLMultiArray(shape:[625,1, 1], dataType:MLMultiArrayDataType.double) else {
-            fatalError("Unexpected runtime error. MLMultiArray")
-        }
-        guard let mlMultiArray2 = try? MLMultiArray(shape:[3,224, 224], dataType:MLMultiArrayDataType.double) else {
-            fatalError("Unexpected runtime error. MLMultiArray")
-        }
-        guard let mlMultiArray3 = try? MLMultiArray(shape:[3,224, 224], dataType:MLMultiArrayDataType.double) else {
-            fatalError("Unexpected runtime error. MLMultiArray")
-        }
-        guard let mlMultiArray4 = try? MLMultiArray(shape:[3,224, 224], dataType:MLMultiArrayDataType.double) else {
-            fatalError("Unexpected runtime error. MLMultiArray")
-        }
-        guard let predict = try? myModel.prediction(facegrid: mlMultiArray1, image_face: mlMultiArray2, image_left: mlMultiArray3, image_right: mlMultiArray4) else {
-            fatalError("Unexpected runtime error. MLMultiArray")
-        }
-        
-        print(mlMultiArray1[0])
-        let max = predict.fc3.count
-        print(max)
-        for index in 0...5{
-            print(predict.fc3[index])
-        }
+//        guard let mlMultiArray1 = try? MLMultiArray(shape:[625,1, 1], dataType:MLMultiArrayDataType.double) else {
+//            fatalError("Unexpected runtime error. MLMultiArray")
+//        }
+//        guard let mlMultiArray2 = try? MLMultiArray(shape:[3,224, 224], dataType:MLMultiArrayDataType.double) else {
+//            fatalError("Unexpected runtime error. MLMultiArray")
+//        }
+//        guard let mlMultiArray3 = try? MLMultiArray(shape:[3,224, 224], dataType:MLMultiArrayDataType.double) else {
+//            fatalError("Unexpected runtime error. MLMultiArray")
+//        }
+//        guard let mlMultiArray4 = try? MLMultiArray(shape:[3,224, 224], dataType:MLMultiArrayDataType.double) else {
+//            fatalError("Unexpected runtime error. MLMultiArray")
+//        }
+//        guard let predict = try? myModel.prediction(facegrid: mlMultiArray1, image_face: mlMultiArray2, image_left: mlMultiArray3, image_right: mlMultiArray4) else {
+//            fatalError("Unexpected runtime error. MLMultiArray")
+//        }
+//
+//        print(mlMultiArray1[0])
+//        let max = predict.fc3.count
+//        print(max)
+//        for index in 0...5{
+//            print(predict.fc3[index])
+//        }
     }
 
     override func didReceiveMemoryWarning() {
