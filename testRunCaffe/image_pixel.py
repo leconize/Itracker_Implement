8e8f21b15ec111ed713d905600d3a2fd78d46404
Leconize <g.supavit@gmail.com>
2018-04-24 10:20:08 +0700

    remove test result folder

--- testRunCaffe/image_pixel.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-im = Image.open("faceScaleImage.png") #Can be many different formats.
-pix = im.load()
-x, y = im.size
-for i in range(1):
-    for j in range(20):
-        print(pix[j,i])
--- testRunCaffe/inspect_model.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import Model_pb2
-
-model = Model_pb2.Model()
-
-with open("Itracker.mlmodel", "rb") as f:
-    model.ParseFromString(f.read())
